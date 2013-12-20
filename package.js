d74b2bac7a8d7053e22fef4552b3b0f220c79ad6
root <root@booking.(none)>
2013-12-20 02:27:08 +0000

    initial test 2

--- package.js
+++ package.js
@@ -1,2 +1,15 @@
-// JavaScript Document
+Package.describe({
+  summary: "eWay Gateway, an Integration module for accepting payments through the eWay gateway. See http://www.eway.com.au."
+});
 
+var both = ['client', 'server']
+
+Package.on_use(function (api) {
+
+  api.add_files('lib/eway/eway.js', 'server');
+
+  if (typeof api.export !== 'undefined') {
+    api.export('eWay', 'server');
+  }
+  
+});
